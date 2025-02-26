faf3f3024d8875dc68e5958f54e78b9f0403763f
Davi Staaks <staaks77@gmail.com>
2025-02-26 18:20:56 -0300

    Requirements atualizado

--- src/1_Home.py
+++ src/1_Home.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("streamlit_fifa/datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv("../datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
@@ -13,10 +13,11 @@ if "data" not in st.session_state:
 st.markdown("# FIFA23 OFFICIAL DATASET")
 st.sidebar.markdown("Desenvolvido por [Davi Staaks](https://www.linkedin.com/in/davi-staaks-23b85620a/) no curso da Asimov")
 
-btn = st.button("Acesse os dados do Kaggle")
+btn = st.link_button("Acesse os dados do Kaggle",
+                     "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/")
+    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 st.markdown("""
             *** Descrição do meu conjunto de dados ***
